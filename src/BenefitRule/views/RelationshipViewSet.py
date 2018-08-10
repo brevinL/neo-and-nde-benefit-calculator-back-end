4e80f178133ff58171feadfaf3789a00d49861b1
Brian Phan <brian.phan@ssa.gov>
2018-08-10 15:51:39 -0400

    b

--- src/BenefitRule/views/RelationshipViewSet.py
+++ src/BenefitRule/views/RelationshipViewSet.py
@@ -1,8 +1,21 @@
 from rest_framework import status, viewsets, serializers
 from BenefitRule.models import Relationship
 from BenefitRule.serializers import RelationshipSerializer
+# from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics
+from NEOandNDEBenefitCalculator.models import Respondent
+from django.db.models import Q
 
 # fix to able to post only
-class RelationshipViewSet(viewsets.ModelViewSet):
-	queryset = Relationship.objects.all()
+class RelationshipViewSet(viewsets.ViewSetMixin, generics.ListAPIView):
 	serializer_class = RelationshipSerializer
+
+	def get_queryset(self):
+		queryset = Relationship.objects.all()
+		if self.request.method == 'GET':
+			respondent_id = self.request.query_params.get('respondent_id', None)
+			if respondent_id:
+				respondent = Respondent.objects.get(id=respondent_id)
+				print(respondent)
+				queryset = Relationship.objects.filter(Q(person1=respondent) | Q(person2=respondent), relationship_type=Relationship.MARRIED)
+		return queryset
